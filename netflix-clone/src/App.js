d7d196c896803dfbeb17fedc4641f27c2d3974ee
fanuael <tilahunalenee@gmail.com>
2024-12-27 15:56:08 +0300

    first commit

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';  // Make sure it's './App.css' not './App'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
